17adb8c817bd8b512e507b707f40e0388897fdd0
Daniel Schauenberg <d@unwiredcouch.com>
2014-05-24 12:42:40 -0400

    update example config

--- config.example.php
+++ config.example.php
@@ -1,5 +1,17 @@
 <?php
 
-$CONFIG = array(
+$CONFIG = [
+    'title' => "cool dashboards",
+    'navitems' => [
+        'Hosts' => '/hosts.php',
+        'Graphite' => '/graphite.php'
+    ],
     'graphitehost' => "https://graphite.example.com",
-    );
+    'hosts' => [
+        "foo.example.com" => [
+            "cpus" => 2,
+            "apache" => true,
+            "filesystems" => [ 'root', 'var', ]
+            ]
+        ]
+    ];
