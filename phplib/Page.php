2ace9f021b59e3bdec18dc652f433f5f22c0a1e7
Daniel Schauenberg <d@unwiredcouch.com>
2015-11-17 22:07:34 -0500

    restructure Page.php
    This includes all of the previously random functionality into a Yagd\Page
    object with associated methods. Making the code structure a lot cleaner and
    adding the possibility to write tests for all functionalities.

--- phplib/Page.php
+++ phplib/Page.php
@@ -1,23 +1,240 @@
 <?php
-require_once("../config.php");
-require_once("../phplib/GraphiteGraph.php");
-include_once("header.php");
 
-use Yagd\GraphiteGraph;
-?>
-<div class="row">
-<?php
-    foreach ( $metrics as $metric) {
+namespace Yagd;
 
-        $graph = new GraphiteGraph($CONFIG['graphite']['host'], $_GET["from"],
-                                   null, $CONFIG['graphite']['hidelegend']);
-        echo('<div class="col-md-4">');
-        $graph->render($metric);
-        echo('</div>');
+class Page {
+
+    protected $config = [];
+    protected $default_times = [
+        '1hour', '4hours', '12hours',
+        '1day', '2days',
+        '1week',
+        '1month', '3months', '6months',
+        '1year'
+    ];
+    protected $default_time = "4hours";
+    protected $select_box = null;
+
+    function __construct($config) {
+        $this->config = $config;
+        if (!isset($this->config["graphite"]["hidelegend"])) {
+            $this->config["graphite"]["hidelegend"] = true;
+        }
+        if (isset($_GET["from"])) {
+            $this->from = $_GET["from"];
+        } else {
+            $this->from = "-" . $this->default_time;
+        }
     }
-?>
-</div>
 
-<?php
-include_once("footer.php");
-?>
+    /**
+     * helper function to build a generic row of Graphite img tag metrics
+     *
+     * Parameters
+     *  $metrics - array of Graphite metrics
+     *
+     * Returns the HTML for the graphs
+     */
+    function build_page_for_metrics($metrics) {
+        $ret = '<div class="row">';
+            foreach ( $metrics as $metric) {
+
+                $graph = new GraphiteGraph($this->config['graphite']['host'],
+                    $this->from, null,
+                    $this->config['graphite']['hidelegend']);
+
+                $ret .= '<div class="col-md-4">';
+                $ret .= $graph->build_graph_img_tag($metric);
+                $ret .= '</div>';
+            }
+        $ret .= '</div>';
+        return $ret;
+    }
+
+    /**
+     * helper function to generically render a full page
+     *
+     * Parameters:
+     *  $metrics - array of graphite metrics to render
+     */
+    function render_full_page_with_metrics($metrics) {
+        print $this->get_header();
+        print $this->build_page_for_metrics($metrics);
+        print $this->get_footer();
+    }
+
+    /**
+     * helper function to return bootstrap compatible attribute data if the
+     * current URI matches the passed in one
+     *
+     * Parameters
+     *  $requestUri - URI to match
+     *
+     * Returns class='active' or the empty string
+     */
+    function get_active_class_if_request_matches($requestUri)
+    {
+        $ret = "";
+        if ($_SERVER['REQUEST_URI'] == $requestUri) {
+            $ret = "class='active'";
+        }
+
+        return $ret;
+    }
+
+    /**
+     * get a select box for an array of times
+     *
+     * Parameters
+     *  $times - array of times
+     *
+     * Returns the HTML for the full select box as a string
+     */
+    function get_time_select_box($times = null) {
+
+        if (is_null($times)) {
+            $times = $this->default_times;
+        }
+
+        $ret = "";
+        $ret .= "<form method='get' action='{$_SERVER['REQUEST_URI']}' ";
+        $ret .= "style='margin-top: 15px'class='pull-right'>";
+        $ret .= "<select name='from' onchange='this.form.submit()'>" . PHP_EOL;
+        foreach ($times as $timefrom) {
+            $current = $this->from ?: "-{$this->default_time}";
+            $selected = ($current == "-${timefrom}") ? " selected" : "";
+            $ret .= "<option ";
+            $ret .= "value='-{$timefrom}'{$selected}>";
+            $ret .= "{$timefrom}</option>";
+            $ret .= PHP_EOL;
+        }
+
+        $ret .= "</select> </form>";
+
+        return $ret;
+
+    }
+
+    /**
+     * Set the select box property
+     *
+     * Parameters
+     *  $select_box - HTML for the select box
+     */
+    function set_select_box($select_box) {
+        $this->select_box = $select_box;
+    }
+
+
+    /**
+     * Get the Yagd HTML header for a page. This can be passed in a couple of
+     * parameters to get a customized page. However by default all parameters
+     * are taken from the Page object configuration.
+     *
+     * Parameters
+     *  $title     - title of the current page
+     *  $nav_items - items to show in the nav bar
+     *  $selectbox - select box HTML to show
+     *  $times     - array of times for the drop down
+     *
+     *  Returns the header data as a string
+     */
+    function get_header($title = null, $nav_items = [],
+                        $selectbox = null, $times = null) {
+
+        if (is_null($title)) {
+            $title = $this->config["title"];
+        }
+        $nav_bar = '<li' . $this->get_active_class_if_request_matches("/") .'>';
+        $nav_bar .= '<a href="/">Home</a></li>';
+        foreach ($nav_items as $name => $url) {
+            $nav_bar .= PHP_EOL;
+            $nav_bar .= "<li" . $this->get_active_class_if_request_matches($url) .">";
+            $nav_bar .= "<a href='{$url}'>{$name}</a></li>";
+        }
+        if (!is_null($selectbox)) {
+            $selectbox_html = "<div class='nav navbar-nav navbar-right'>";
+            $selectbox_html .= $selectbox . "</div>";
+        } elseif (!is_null($this->select_box)) {
+            $selectbox_html = "<div class='nav navbar-nav navbar-right'>";
+            $selectbox_html .= $this->select_box . "</div>";
+        } else {
+            $selectbox_html = "<!-- select box would appear here -->";
+        }
+
+        $timeselect = $this->get_time_select_box($times);
+
+        $header = <<<"EOD"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboards</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+    <style>
+        body {
+            padding-top: 65px;
+        }
+    </style>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">{$title} </a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            {$nav_bar}
+          </ul>
+            <div class='nav navbar-nav navbar-left'>
+            {$timeselect}
+            </div>
+            {$selectbox_html}
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  <div class="container">
+EOD;
+        return $header;
+    }
+
+    /**
+     * get footer for page. This should always be called if get_header() has
+     * been called before as they complement each other.
+     *
+     * Returns the footer as a string
+     */
+    function get_footer() {
+        $footer = <<<"EOF"
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
+EOF;
+        return $footer;
+    }
+}
---
+++ tests/Page_tests.php
@@ -0,0 +1,254 @@
+<?php
+require_once("phplib/Page.php");
+
+use Yagd\Page;
+
+class PageTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp() {
+        $_SERVER['REQUEST_URI'] = "/foo";
+
+        $this->page = new Page(["title" => "foo",
+                                "graphite" => ["host" => "http://grpht.exmpl.com"]]);
+    }
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf('Yagd\Page', $this->page);
+    }
+
+    public function testInstantiationWithMockGET()
+    {
+        $_GET["from"] = "-1hour";
+        $this->page = new Page([]);
+        $this->assertInstanceOf('Yagd\Page', $this->page);
+    }
+
+    public function testGetFooter() {
+        $expected = <<<"EOF"
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
+EOF;
+        $this->assertEquals($expected, $this->page->get_footer());
+
+    }
+
+    public function testGetActiveClassIfRequestMatches() {
+
+        $this->assertEquals("class='active'", $this->page->get_active_class_if_request_matches("/foo"));
+    }
+
+    public function testGetTimeSelectBox() {
+        $times = [
+            '1hour', '4hours', '12hours',
+            '1day', '2days',
+            '1week',
+            '1month', '3months', '6months',
+            '1year'
+        ];
+        $expected = <<<EOE
+<form method='get' action='/foo' style='margin-top: 15px'class='pull-right'><select name='from' onchange='this.form.submit()'>
+<option value='-1hour'>1hour</option>
+<option value='-4hours' selected>4hours</option>
+<option value='-12hours'>12hours</option>
+<option value='-1day'>1day</option>
+<option value='-2days'>2days</option>
+<option value='-1week'>1week</option>
+<option value='-1month'>1month</option>
+<option value='-3months'>3months</option>
+<option value='-6months'>6months</option>
+<option value='-1year'>1year</option>
+</select> </form>
+EOE;
+        $ret = $this->page->get_time_select_box($times);
+
+        $this->assertEquals($expected, $ret);
+    }
+
+    public function testGetTimeSelectBoxWithDefaultTimes() {
+        $expected = <<<EOE
+<form method='get' action='/foo' style='margin-top: 15px'class='pull-right'><select name='from' onchange='this.form.submit()'>
+<option value='-1hour'>1hour</option>
+<option value='-4hours' selected>4hours</option>
+<option value='-12hours'>12hours</option>
+<option value='-1day'>1day</option>
+<option value='-2days'>2days</option>
+<option value='-1week'>1week</option>
+<option value='-1month'>1month</option>
+<option value='-3months'>3months</option>
+<option value='-6months'>6months</option>
+<option value='-1year'>1year</option>
+</select> </form>
+EOE;
+        $ret = $this->page->get_time_select_box();
+
+        $this->assertEquals($expected, $ret);
+    }
+
+    public function testGetHeader() {
+        $expected = <<<"EOD"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboards</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+    <style>
+        body {
+            padding-top: 65px;
+        }
+    </style>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">foo </a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="/">Home</a></li>
+<li><a href='/bla'>bla</a></li>
+          </ul>
+            <div class='nav navbar-nav navbar-left'>
+            <form method='get' action='/foo' style='margin-top: 15px'class='pull-right'><select name='from' onchange='this.form.submit()'>
+<option value='-1hour'>1hour</option>
+<option value='-4hours' selected>4hours</option>
+<option value='-12hours'>12hours</option>
+<option value='-1day'>1day</option>
+<option value='-2days'>2days</option>
+<option value='-1week'>1week</option>
+<option value='-1month'>1month</option>
+<option value='-3months'>3months</option>
+<option value='-6months'>6months</option>
+<option value='-1year'>1year</option>
+</select> </form>
+            </div>
+            <div class='nav navbar-nav navbar-right'>selectbox here</div>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  <div class="container">
+EOD;
+        $nav_items = ["bla" => "/bla"];
+        $selectbox = "selectbox here";
+        $ret = $this->page->get_header("foo", $nav_items, $selectbox);
+        $this->assertEquals($expected, $ret);
+        $this->page->set_select_box($selectbox);
+        $ret = $this->page->get_header("foo", $nav_items);
+        $this->assertEquals($expected, $ret);
+        $ret = $this->page->get_header("foo", $nav_items);
+        $this->assertEquals($expected, $ret);
+    }
+
+    public function testGetHeaderWithoutSelectBox() {
+        $expected = <<<"EOD"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboards</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+    <style>
+        body {
+            padding-top: 65px;
+        }
+    </style>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">foo </a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="/">Home</a></li>
+<li><a href='/bla'>bla</a></li>
+          </ul>
+            <div class='nav navbar-nav navbar-left'>
+            <form method='get' action='/foo' style='margin-top: 15px'class='pull-right'><select name='from' onchange='this.form.submit()'>
+<option value='-1hour'>1hour</option>
+<option value='-4hours' selected>4hours</option>
+<option value='-12hours'>12hours</option>
+<option value='-1day'>1day</option>
+<option value='-2days'>2days</option>
+<option value='-1week'>1week</option>
+<option value='-1month'>1month</option>
+<option value='-3months'>3months</option>
+<option value='-6months'>6months</option>
+<option value='-1year'>1year</option>
+</select> </form>
+            </div>
+            <!-- select box would appear here -->
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  <div class="container">
+EOD;
+        $nav_items = ["bla" => "/bla"];
+        $ret = $this->page->get_header("foo", $nav_items);
+        $this->assertEquals($expected, $ret);
+    }
+
+    public function testBuildPageForMetrics() {
+        $expct = '<div class="row"><div class="col-md-4"><img src="http://grpht.exmpl.com/render?width=400&from=-4hours&target=foo&hideLegend=true"></img></div><div class="col-md-4"><img src="http://grpht.exmpl.com/render?width=400&from=-4hours&target=bla&hideLegend=true"></img></div></div>';
+        $ret = $this->page->build_page_for_metrics(["foo", "bla"]);
+        $this->assertEquals($expct, $ret);
+    }
+
+    public function testRenderFullPageWithMetrics() {
+        // we don't care about the output here as it's tested in individual
+        // tests already, thus we don't assert anything and clean the output
+        // buffer. However we still want to call the method as part of the
+        // test suite.
+        ob_start();
+        $this->page->render_full_page_with_metrics(["foo", "bla"]);
+        ob_end_clean();
+    }
+
+}
